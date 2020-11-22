a8745a0f94673f5185c28cca4318b7ffca3d4db7
vincentwetzel <vincentwetzel3@gmail.com>
2020-11-21 22:28:55 -0700

    ~QOL updates. This should be the final version unless I add features.

--- main.py
+++ main.py
@@ -13,27 +13,25 @@ import calendar
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/contacts']
-zodiac_file_location = None
+ZODIAC_FILE_LOC = None
 with open("zodiac_file_location.txt") as f:
-    zodiac_file_location = f.readline().strip()
-zodiac_names: Dict[str, str] = dict()
-"""{ person: date }"""
+    ZODIAC_FILE_LOC = f.readline().strip()
+zodiac_names_and_birthdays: Dict[str, Dict[str, int]] = dict()
+"""{ Person : { day/month/year : int} }"""
 
 contacts_analyzed = 0
 contacts_updated = 0
+known_birthdays_count = 0
 
 
 def main():
-    """Shows basic usage of the People API.
-    Prints the name of the first 10 connections.
-    """
-
-    pd = pandas.read_excel(zodiac_file_location)
+    pd = pandas.read_excel(ZODIAC_FILE_LOC)
     for idx, row in pd.iterrows():
-        zodiac_names[row["Name"]] = dict()
-        zodiac_names[row["Name"]]["day"] = row["Day"]
-        zodiac_names[row["Name"]]["month"] = list(calendar.month_name).index(row["Month"])  # Convert month name to int
-        zodiac_names[row["Name"]]["year"] = row["Year"]
+        zodiac_names_and_birthdays[row["Name"]] = dict()
+        zodiac_names_and_birthdays[row["Name"]]["day"] = row["Day"]
+        zodiac_names_and_birthdays[row["Name"]]["month"] = list(calendar.month_name).index(
+            row["Month"])  # Convert month name to int
+        zodiac_names_and_birthdays[row["Name"]]["year"] = row["Year"]
 
     creds = init_credentials()
     service = build('people', 'v1', credentials=creds, cache_discovery=False)
@@ -43,25 +41,29 @@ def main():
         # people.connections.list provides a List of authenticated user's contacts
         results = service.people().connections().list(
             resourceName='people/me',
-            pageSize=500,
+            pageSize=1000,
             pageToken=page_token,
             personFields='names,emailAddresses,nicknames,birthdays').execute()
 
         connections = results.get('connections', [])
         do_stuff_to_connections_list(service, connections)
 
+        # See if there are more pages to process
         if "nextPageToken" in results:
             page_token = results["nextPageToken"]
         else:
+            # Done analyzing all contacts, kill the loop
             break
 
-    print("Total contacts analyzed: " + str(contacts_analyzed))
-    print("Total contacts updated: " + str(contacts_updated))
+    logging.info("Total contacts analyzed: " + str(contacts_analyzed))
+    logging.info("Total contacts updated: " + str(contacts_updated))
+    logging.info("Total known birthdays: " + str(known_birthdays_count))
 
 
 def do_stuff_to_connections_list(service, connectionsList: list):
     global contacts_updated
     global contacts_analyzed
+    global known_birthdays_count
 
     for person in connectionsList:
         # Try to get a Person's name.
@@ -79,15 +81,19 @@ def do_stuff_to_connections_list(service, connectionsList: list):
 
         if birthdays and names:
             try:
-                logging.info("NAME: " + name + "\t\tBirthday YEAR: " + str(birthday["year"]))
+                logging.info("NAME: " + name + "\t\tBirthday: " + str(birthday["month"]) + "/" + str(
+                    birthday["day"]) + "/" + str(birthday["year"]))
             except Exception as e:
                 logging.debug("NAME:" + name + " caused Exception: " + str(e))
 
+            known_birthdays_count = known_birthdays_count + 1
+
         elif names:
             # I have a name but no birthday for this person. These are the ones we want to fix.
-            if name in zodiac_names.keys():
-                logging.debug("Adding birthday (" + str(zodiac_names[name]["month"]) + "/" + str(
-                    zodiac_names[name]["day"]) + "/" + str(zodiac_names[name]["year"]) + ") for " + name)
+            if name in zodiac_names_and_birthdays.keys():
+                logging.debug("Adding birthday (" + str(zodiac_names_and_birthdays[name]["month"]) + "/" + str(
+                    zodiac_names_and_birthdays[name]["day"]) + "/" + str(
+                    zodiac_names_and_birthdays[name]["year"]) + ") for " + name)
 
                 # logging.debug("BEFORE:")
                 # logging.debug(str(person))
@@ -98,11 +104,11 @@ def do_stuff_to_connections_list(service, connectionsList: list):
                 birthday_obj["metadata"]["primary"] = True
                 birthday_obj["metadata"]["verified"] = True
                 birthday_obj["date"] = dict()
-                birthday_obj["text"] = str(zodiac_names[name]["month"]) + "/" + str(
-                    zodiac_names[name]["day"]) + "/" + str(zodiac_names[name]["year"])
-                birthday_obj["date"]["year"] = zodiac_names[name]["year"]
-                birthday_obj["date"]["month"] = zodiac_names[name]["month"]
-                birthday_obj["date"]["day"] = zodiac_names[name]["day"]
+                birthday_obj["text"] = str(zodiac_names_and_birthdays[name]["month"]) + "/" + str(
+                    zodiac_names_and_birthdays[name]["day"]) + "/" + str(zodiac_names_and_birthdays[name]["year"])
+                birthday_obj["date"]["year"] = zodiac_names_and_birthdays[name]["year"]
+                birthday_obj["date"]["month"] = zodiac_names_and_birthdays[name]["month"]
+                birthday_obj["date"]["day"] = zodiac_names_and_birthdays[name]["day"]
                 person["birthdays"] = birthday_obj
 
                 # logging.debug("AFTER:")
@@ -115,9 +121,10 @@ def do_stuff_to_connections_list(service, connectionsList: list):
                 ).execute()
 
                 contacts_updated = contacts_updated + 1
+                known_birthdays_count = known_birthdays_count + 1
             else:
                 pass
-                # print(name)
+                # logging.debug(name)
         else:
             logging.debug("names came up empty for some reason.")
             logging.debug(str(person))
